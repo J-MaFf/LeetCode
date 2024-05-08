38957add6f5873e0f0142fdc78fad5922a9eb319
Joey Maffiola <7maffiolajoey@gmail.com>
2024-05-07 23:27:49 -0500

    Close, got the right number but with an extra 0 at the end.

--- (2) Add two numbers/ListNode.java
+++ (2) Add two numbers/ListNode.java
@@ -15,11 +15,6 @@ class Solution {
     static int carry;
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode current = new ListNode(0);
-        ListNode sum = current; // sum is the head of the list
-        ListNode nextNode;
-        int digitSum;
-        int carry;
         while (l1 != null && l2 != null) { // While l1 and l2 have more nodes
 
             // Numbers are in reverse
@@ -31,7 +26,8 @@ class Solution {
                 digitSum = digitSum % 10; // modulo
             }
             nextNode = new ListNode(carry);
-            current = new ListNode(digitSum, nextNode); 
+            current.val = digitSum;
+            current.next = nextNode;
             current = nextNode;
             l1 = l1.next; // Advance nodes
             l2 = l2.next;
@@ -45,16 +41,8 @@ class Solution {
             addSingleNode(l2.val);
             l2 = l2.next;
         }
-        ListNode reverseSum = sum;
-        // Reverse sum calculated, print
-        System.out.print("[");
-        while (sum != null) {
-            System.err.print(sum.val + ",");
-            sum = sum.next;
-        }
-        System.out.print("]");
 
-        return reverseSum;
+        return sum;
 
     }
 
