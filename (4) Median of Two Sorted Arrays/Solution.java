dd0646e12a2a1ff8de9ed09a9bb870c12a592ac4
Joey Maffiola <7maffiolajoey@gmail.com>
2024-05-20 22:31:23 -0500

    Trying to figure out my strat.

--- (4) Median of Two Sorted Arrays/Solution.java
+++ (4) Median of Two Sorted Arrays/Solution.java
@@ -5,6 +5,12 @@
 
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        return -1.0;
+        int totalLength = nums1.length + nums2.length;
+
+        if (totalLength % 2 == 0) { // Even
+
+        } else { // odd
+            //
+        }
     }
 }
