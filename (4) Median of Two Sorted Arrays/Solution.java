269ff0a75f68b9e92ef74b99e6291f7effb99e46
Joey Maffiola <7maffiolajoey@gmail.com>
2024-06-27 11:30:48 -0500

    Starting fresh. First implementation: Make sure nums1 is the smaller array.

--- (4) Median of Two Sorted Arrays/Solution.java
+++ (4) Median of Two Sorted Arrays/Solution.java
@@ -5,16 +5,11 @@
 
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int totalLength = nums1.length + nums2.length;
-
-        if (totalLength == 1) {
-            // There is only one element, so that is the median
-            return (nums1.length == 0) ? nums2[0] : nums1[0];
-        }
-
-        // Make sure nums1 is the smaller array
-        if (nums1.length > nums2.length) {
-            return findMedianSortedArrays(nums2, nums1);
+        int m = nums1.length;
+        int n = nums2.length;
+        // Confirm that nums1 is the smaller array
+        if (m > n) {
+            findMedianSortedArrays(nums2, nums1);
         }
 
     }
