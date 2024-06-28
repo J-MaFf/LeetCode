b012af5b6803c7ea3c01ef913ffbd0e9b37fa26c
Joey Maffiola <7maffiolajoey@gmail.com>
2024-06-28 11:11:52 -0500

    Question 4 complete. This one was way too hard, I was unable to come up with the algo on my own. I had to look at the editorial and see the intuition behind this. The part that tripped me up the most was that I was unable to combine the arrays because of the Time complexity constraint.

--- (4) Median of Two Sorted Arrays/Solution.java
+++ (4) Median of Two Sorted Arrays/Solution.java
@@ -5,11 +5,10 @@
 
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
+        int m = nums1.length, n = nums2.length;
         // Confirm that nums1 is the smaller array
         if (m > n) {
-            findMedianSortedArrays(nums2, nums1);
+            return findMedianSortedArrays(nums2, nums1);
         }
         // Define search space
         int left = 0;
@@ -26,28 +25,23 @@ class Solution {
             int maxLeftB = (partitionB - 1 < 0) ? Integer.MIN_VALUE : nums2[partitionB - 1];
             int minRightB = (partitionB >= n) ? Integer.MAX_VALUE : nums2[partitionB];
 
-            // Compare and recalculate
-            if (maxLeftA > minRightB) { // maxLeftA is too large to be in the smaller half
-                right = partitionA - 1; // move to the left half of the search space
-            }
-            if (maxLeftB > minRightA) { // too far on the left side for partitionA
-                left = partitionA + 1; // go to the right half of the search space
-            }
-            partitionA = (left + right) / 2;
-            partitionB = (m + n + 1) / 2 - partitionA;
-
             if (maxLeftA <= minRightB && maxLeftB <= minRightA) { // correct partitioning
-                int answer = 0;
                 if ((m + n) % 2 == 0) { // Even
                     // The median value is the average of the maximum value of
                     // the smaller half and the minimum value of the larger half
-                    answer = (Math.max(maxLeftA, maxLeftB) + Math.min(minRightA, minRightB)) / 2;
+                    return ((double) Math.max(maxLeftA, maxLeftB) + (double) Math.min(minRightA, minRightB)) / 2;
                 } else { // Odd
                     // the median value is the maximum value of the smaller half
-                    answer = Math.max(maxLeftA, maxLeftB);
+                    return Math.max(maxLeftA, maxLeftB);
                 }
-                return answer;
+            }
+            // Compare and recalculate
+            else if (maxLeftA > minRightB) { // maxLeftA is too large to be in the smaller half
+                right = partitionA - 1; // move to the left half of the search space
+            } else { // too far on the left side for partitionA
+                left = partitionA + 1; // go to the right half of the search space
             }
         }
-    }
+        return 0.0;
+    } // end of method
 }
