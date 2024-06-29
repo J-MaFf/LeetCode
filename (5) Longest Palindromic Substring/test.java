9e38b5d47ac3f6dd58621f9ff9a8726a7828611c
Joey Maffiola <7maffiolajoey@gmail.com>
2024-06-28 21:38:37 -0500

    Adding tests

---
+++ (5) Longest Palindromic Substring/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println("babad\n" + s.longestPalindrome("babad"));
+
+    }
+}
